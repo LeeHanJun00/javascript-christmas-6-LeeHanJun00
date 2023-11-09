2deb7ce92c3459d298b08332ef1cd759c82e3ac4
leehanjun00 <yeehj20@naver.com>
2023-11-10 02:06:05 +0900

    feat: 주문메뉴 출력 기능

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import InputView from './views/InputView.js';
 import OutputView from './views/OutputView.js';
 import Menu from './Menu.js';
+
 class App {
   async run() {
     OutputView.printStart();
+
     const visitDate = await InputView.inputDate();
+
     const menu = new Menu();
     const menuLsit = await menu.ipnutMenu();
-    console.log(menuLsit);
+
+    OutputView.printMenu(menuLsit);
+    const totalPrice = OutputView.totalAmount(menuLsit);
   }
 }
 
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,14 +1,31 @@
 import { Console } from '@woowacourse/mission-utils';
+import { MENU } from '../constants/Constant.js';
 
 const OutputView = {
   printStart() {
     Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+
+  printMenu(menuLsit) {
+    Console.print('\n<주문 메뉴>');
+    for (const menu in menuLsit) {
+      Console.print(`${menu} ${menuLsit[menu]}개`);
+    }
   },
-  // ...
+
+  totalAmount(menuLsit) {
+    Console.print('\n<할인 전 총주문 금액>');
+    let totalPrice = 0;
+    for (const item in menuLsit) {
+      if (MENU.hasOwnProperty(item)) {
+        totalPrice += MENU[item] * menuLsit[item];
+      }
+    }
+    const totalAmount = totalPrice.toLocaleString();
+    Console.print(`${totalAmount}원`);
+
+    return totalPrice;
+  },
 };
 
 export default OutputView;
