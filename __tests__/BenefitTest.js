899560fdfd2b74ddc8bd9b780c07070757360d1b
leehanjun00 <yeehj20@naver.com>
2023-11-13 00:46:49 +0900

    test: 특별할인 혜택 테스트

--- __tests__/BenefitTest.js
+++ __tests__/BenefitTest.js
@@ -76,3 +76,19 @@ describe('평일,주말 혜택 테스트', () => {
     expect(await benefit.checkWeekendDiscount(visitDate, menuLsit)).toBe(output);
   });
 });
+
+describe('특별할인 혜택 테스트', () => {
+  const visitDate = [[3], [10], [17], [24], [25], [31]];
+  test.each(visitDate)('달력에 별이있는 날짜 선택시 1000원 할인', async (ipnut) => {
+    const output = 1000;
+    const benefit = new Benefit();
+    expect(benefit.checkSpecialDate(ipnut)).toBe(output);
+  });
+
+  test('달력에 별이없는 날짜 선택시 할인혜택 0원', async () => {
+    const visitDate = 15;
+    const output = 0;
+    const benefit = new Benefit();
+    expect(benefit.checkSpecialDate(visitDate)).toBe(output);
+  });
+});
